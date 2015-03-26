9eaeda1b95570c07a3789fa7009ecf72d14da98b
Doug Bennett <dben89x@gmail.com>
2015-03-26 02:54:58 -0600

    changed a few styling things

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,16 +4,3 @@
 //= require_tree .
 //= require bootstrap/collapse
 //= require bootstrap/transition
-$(document).ready(function(){
-  var myLatLng = new google.maps.LatLng(40.0293099,-105.2399774);
-  var myOptions = {
-    zoom: 14,
-    center: myLatLng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    scrollwheel: false,
-    //disableDefaultUI: true,
-  };
-  map = new google.maps.Map(
-    document.getElementsByClassName('map')[0], myOptions
-  )
-});
